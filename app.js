995afb6e960c9a21a6cafe197967410e45faabd9
@Drikkii <chevpavel@gmail.com>
2024-01-06 19:47:50 +0300

    refactor:(basic-3.4) I change the tags to classes

--- app.js
+++ app.js
@@ -33,20 +33,22 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="task-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task-input";
+    listItem.className="task-item";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
-
+    deleteButtonImg.className = "img-delete";
 
     //and appending.
     listItem.appendChild(checkBox);
